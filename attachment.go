79b12f67ad864382fa329668f9e60664e097a695
Pauline ESPALIEU <paulineespalieu@mbppaulespalieu.home>
2024-08-26 14:45:16 +0200

    change elementOptions to only take into consideration deployed, deleted, creation_error and delete_error

--- attachment.go
+++ attachment.go
@@ -7,28 +7,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"slices"
 	"strings"
 
 	"github.com/intercloud/autonomi-sdk/models"
 )
 
-func checkAttachmentAdministrativeState(ctx context.Context, c *Client, workspaceID, attachmentID string, state models.AdministrativeState) (*models.Attachment, bool) {
+func checkAttachmentFinishedTask(ctx context.Context, c *Client, workspaceID, attachmentID string, waiterOptions []models.AdministrativeState) (*models.Attachment, bool) {
 	attachment, err := c.GetAttachment(ctx, workspaceID, attachmentID)
 	if err != nil {
 		// if wanted state is deleted and the attachment is in this state, api has returned 404
-		if state == models.AdministrativeStateDeleted && strings.Contains(err.Error(), "status: 404") {
-			return nil, true
+		if slices.Equal(waiterOptions, waitUntilElementUndeployed) {
+			if strings.Contains(err.Error(), "status: 404") {
+				return nil, true
+			}
 		}
 		log.Printf("an error occurs when getting attachment, err: %s" + err.Error())
 		return nil, false
 	}
 
-	return attachment, attachment.State == state
+	return attachment, slices.Contains(waiterOptions, attachment.State)
 }
 
-// CreateAttachment creates asynchronously an attachment. The attachment returned will depend of the administrative state passed in options.
-// If none is passed models.AdministrativeStateDeployed will be set by default.
-// The valid administrative states options for a attachment creation are [models.AdministrativeStateCreationPending, models.AdministrativeStateCreationProceed, models.AdministrativeStateCreationError, models.AdministrativeStateDeployed]
+// CreateAttachment creates asynchronously an attachment. The attachment returned will depend of the passed option.
+// If none is passed the attachment will be returned once created in database with administrative state creation_pending.
+// If the option WithWaitUntilElementDeployed() is passed, the attachment will be returned when its state reach deployed or creation_error.
+// If the option WithWaitUntilElementUndeployed() is passed, it will not be considered hence the attachment returned will be in state creation_pending
 func (c *Client) CreateAttachment(ctx context.Context, payload models.CreateAttachment, workspaceID string, options ...OptionElement) (*models.Attachment, error) {
 	body := new(bytes.Buffer)
 	err := json.NewEncoder(body).Encode(&payload)
@@ -43,14 +47,6 @@ func (c *Client) CreateAttachment(ctx context.Context, payload models.CreateAtta
 	attachmentOptions := &elementOptions{}
 	for _, o := range options {
 		o(attachmentOptions)
-	}
-
-	if attachmentOptions.administrativeState == "" {
-		attachmentOptions.administrativeState = models.AdministrativeStateDeployed
-	}
-
-	if _, ok := validCreationAdministrativeStates[attachmentOptions.administrativeState]; !ok {
-		return nil, ErrCreationAdministrativeState
 	}
 
 	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/accounts/%s/workspaces/%s/attachments", c.hostURL, c.accountID, workspaceID), body)
@@ -69,9 +65,13 @@ func (c *Client) CreateAttachment(ctx context.Context, payload models.CreateAtta
 		return nil, err
 	}
 
-	attachmentPolled, success := WaitForAdministrativeState(ctx, c, workspaceID, attachment.Data.ID.String(), attachmentOptions.administrativeState, checkAttachmentAdministrativeState)
-	if !success {
-		return nil, fmt.Errorf("Attachment did not reach '%s' state in time.", attachmentOptions.administrativeState)
+	var attachmentPolled = &attachment.Data
+	if attachmentOptions.waitUntilElementDeployed {
+		var success bool
+		attachmentPolled, success = WaitUntilFinishedTask(ctx, c, workspaceID, attachment.Data.ID.String(), waitUntilElementDeployed, checkAttachmentFinishedTask)
+		if !success {
+			return nil, fmt.Errorf("Attachment did not reach '%s' state in time.", models.AdministrativeStateDeployed)
+		}
 	}
 
 	return attachmentPolled, nil
@@ -97,21 +97,14 @@ func (c *Client) GetAttachment(ctx context.Context, workspaceID, attachmentID st
 	return &attachment.Data, err
 }
 
-// DeleteAttachment deletes asynchronously a cloud attachment. The attachment returned will depend of the administrative state passed in options.
-// If none is passed models.AdministrativeStateDeleted will be set by default.
-// The valid administrative states options for a attachment creation are [ models.AdministrativeStateDeletePending, models.AdministrativeStateDeleteProceed, models.AdministrativeStateDeleteError, models.AdministrativeStateDeleted]
+// DeleteAttachment deletes asynchronously an attachment. The attachment returned will depend of the option passed.
+// If none is passed the attachment will be returned once the request accepted, its state will be delete_pending
+// If the option WithWaitUntilElementUndeployed() is passed, the attachment won't be returned as it would have been deleted. However, if an error is triggered, an object could be returned with a delete_error state.
+// If the option WithWaitUntilElementDeployed() is passed, it will not be considered hence the attachment returned will be in state delete_pending
 func (c *Client) DeleteAttachment(ctx context.Context, workspaceID, attachmentID string, options ...OptionElement) (*models.Attachment, error) {
 	attachmentOptions := &elementOptions{}
 	for _, o := range options {
 		o(attachmentOptions)
-	}
-
-	if attachmentOptions.administrativeState == "" {
-		attachmentOptions.administrativeState = models.AdministrativeStateDeleted
-	}
-
-	if _, ok := validDeletionAdministrativeStates[attachmentOptions.administrativeState]; !ok {
-		return nil, ErrDeletionAdministrativeState
 	}
 
 	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/accounts/%s/workspaces/%s/attachments/%s", c.hostURL, c.accountID, workspaceID, attachmentID), nil)
@@ -130,9 +123,13 @@ func (c *Client) DeleteAttachment(ctx context.Context, workspaceID, attachmentID
 		return nil, err
 	}
 
-	attachmentPolled, success := WaitForAdministrativeState(ctx, c, workspaceID, attachment.Data.ID.String(), attachmentOptions.administrativeState, checkAttachmentAdministrativeState)
-	if !success {
-		return nil, fmt.Errorf("Attachment did not reach '%s' state in time.", attachmentOptions.administrativeState)
+	var attachmentPolled = &attachment.Data
+	if attachmentOptions.waitUntilElementUndeployed {
+		var success bool
+		attachmentPolled, success = WaitUntilFinishedTask(ctx, c, workspaceID, attachment.Data.ID.String(), waitUntilElementUndeployed, checkAttachmentFinishedTask)
+		if !success {
+			return nil, fmt.Errorf("Attachment did not reach '%s' state in time.", models.AdministrativeStateDeleted)
+		}
 	}
 
 	// If the attachment was deleted and we were waiting for the "deleted" state,
--- elements.go
+++ elements.go
@@ -2,54 +2,43 @@ package autonomisdk
 
 import (
 	"context"
-	"errors"
 	"time"
 
 	"github.com/intercloud/autonomi-sdk/models"
 )
 
 type elementOptions struct {
-	administrativeState models.AdministrativeState
+	waitUntilElementDeployed   bool
+	waitUntilElementUndeployed bool
 }
 type OptionElement func(*elementOptions)
 
-var validCreationAdministrativeStates = map[models.AdministrativeState]struct{}{
-	models.AdministrativeStateCreationPending: {},
-	models.AdministrativeStateCreationProceed: {},
-	models.AdministrativeStateCreationError:   {},
-	models.AdministrativeStateDeployed:        {},
-}
-
-var validDeletionAdministrativeStates = map[models.AdministrativeState]struct{}{
-	models.AdministrativeStateDeletePending: {},
-	models.AdministrativeStateDeleteProceed: {},
-	models.AdministrativeStateDeleteError:   {},
-	models.AdministrativeStateDeleted:       {},
-}
-
 var (
-	ErrCreationAdministrativeState = errors.New("not a valid administrative state for element creation. Try [AdministrativeStateCreationPending, AdministrativeStateCreationProceed, AdministrativeStateCreationError, AdministrativeStateDeployed ]")
-	ErrDeletionAdministrativeState = errors.New("not a valid administrative state for element deletion. Try [AdministrativeStateDeletePending, AdministrativeStateDeleteProceed, AdministrativeStateDeleteError, AdministrativeStateDeleted ]")
+	waitUntilElementDeployed   = []models.AdministrativeState{models.AdministrativeStateCreationError, models.AdministrativeStateDeployed}
+	waitUntilElementUndeployed = []models.AdministrativeState{models.AdministrativeStateDeleteError, models.AdministrativeStateDeleted}
 )
 
-// WithAdministrativeState allows setting a specific administrative state. The node will be returned when it reaches the specified administrative state.
-func WithAdministrativeState(administrativeState models.AdministrativeState) OptionElement {
-	return func(c *elementOptions) {
-		c.administrativeState = administrativeState
+func WithWaitUntilElementDeployed() OptionElement {
+	return func(e *elementOptions) {
+		e.waitUntilElementDeployed = true
 	}
 }
 
-// type CheckForAdministrativeState func(ctx context.Context, c *Client, workspaceID, elementID string, state models.AdministrativeState) bool
+func WithWaitUntilElementUndeployed() OptionElement {
+	return func(e *elementOptions) {
+		e.waitUntilElementUndeployed = true
+	}
+}
 
 type Element interface {
 	*models.Node | *models.Transport | *models.Attachment
 }
 
-func WaitForAdministrativeState[T Element](ctx context.Context, client *Client, workspaceID, elementID string, state models.AdministrativeState, funcToCall func(context.Context, *Client, string, string, models.AdministrativeState) (T, bool)) (T, bool) {
+func WaitUntilFinishedTask[T Element](ctx context.Context, client *Client, workspaceID, elementID string, waiterOptions []models.AdministrativeState, funcToCall func(context.Context, *Client, string, string, []models.AdministrativeState) (T, bool)) (T, bool) {
 	var lastElement T
 	for i := 0; i < client.poll.maxRetry; i++ {
 		// Retrieve the element and check if it is in the required administrative state
-		element, isInDesiredState := funcToCall(ctx, client, workspaceID, elementID, state)
+		element, isInDesiredState := funcToCall(ctx, client, workspaceID, elementID, waiterOptions)
 		lastElement = element
 
 		if isInDesiredState {
