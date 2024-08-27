0b08c9c8954c8cf43ddd90bc8448c335be8b54e4
Guillaume <guillaume.beaumont@intercloud.com>
2024-08-27 11:59:17 +0200

    avec la PJ c'est mieux

--- models/physical_port.go
+++ models/physical_port.go
@@ -1,7 +1,17 @@
 package models
 
+type PhysicalPortProduct struct {
+	Product
+}
+
 type PhysicalPort struct {
 	BaseModel
+	Name               string              `json:"name"`
+	AccountID          string              `json:"accountId"`
+	Product            PhysicalPortProduct `json:"product"`
+	AvailableBandwidth int                 `json:"availableBandwidth"`
+	State              string              `json:"administrativeState"`
+	UsedVLANs          []int64             `json:"usedVlans"`
 }
 
 type PhysicalPortResponse struct {
