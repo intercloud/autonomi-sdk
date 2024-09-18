52edc4776c66d99b048d47fd7e7cec74362763e6
Guillaume BEAUMONT <7782511+gbeaumont93@users.noreply.github.com>
2024-09-18 08:51:47 +0200

    physical port management (#17)

--- models/physical_port.go
+++ models/physical_port.go
@@ -10,10 +10,19 @@ type PhysicalPort struct {
 	AccountID          string              `json:"accountId"`
 	Product            PhysicalPortProduct `json:"product"`
 	AvailableBandwidth int                 `json:"availableBandwidth"`
-	State              string              `json:"administrativeState"`
+	State              AdministrativeState `json:"administrativeState"`
 	UsedVLANs          []int64             `json:"usedVlans"`
 }
 
-type PhysicalPortResponse struct {
+type PhysicalPortSingleResponse struct {
+	Data PhysicalPort `json:"data"`
+}
+
+type PhysicalPortListResponse struct {
 	Data []PhysicalPort `json:"data"`
+}
+
+type CreatePhysicalPort struct {
+	Name    string     `json:"name" binding:"required"`
+	Product AddProduct `json:"product" binding:"required"`
 }
--- port.go
+++ port.go
@@ -1,12 +1,65 @@
 package autonomisdk
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/intercloud/autonomi-sdk/models"
 )
+
+// CreatePhysicalPort creates a physical port in Autonomi platform.
+func (c *Client) CreatePhysicalPort(ctx context.Context, payload models.CreatePhysicalPort) (*models.PhysicalPort, error) {
+	body := new(bytes.Buffer)
+	err := json.NewEncoder(body).Encode(&payload)
+	if err != nil {
+		return nil, err
+	}
+
+	if errV := c.validate.StructCtx(ctx, payload); errV != nil {
+		return nil, errV
+	}
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/accounts/%s/ports", c.hostURL, c.accountID), body)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	physicalPort := models.PhysicalPortSingleResponse{}
+	err = json.Unmarshal(resp, &physicalPort)
+	if err != nil {
+		return nil, err
+	}
+
+	return &physicalPort.Data, nil
+}
+
+func (c *Client) GetPhysicalPort(ctx context.Context, portID string) (*models.PhysicalPort, error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/accounts/%s/ports/%s", c.hostURL, c.accountID, portID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	physicalPort := models.PhysicalPortSingleResponse{}
+	err = json.Unmarshal(resp, &physicalPort)
+	if err != nil {
+		return nil, err
+	}
+
+	return &physicalPort.Data, err
+}
 
 func (c *Client) ListPort(options ...OptionElement) (*[]models.PhysicalPort, error) {
 
@@ -34,11 +87,27 @@ func (c *Client) ListPort(options ...OptionElement) (*[]models.PhysicalPort, err
 		return nil, err
 	}
 
-	ports := models.PhysicalPortResponse{}
+	ports := models.PhysicalPortListResponse{}
 	err = json.Unmarshal(resp, &ports)
 	if err != nil {
 		return nil, err
 	}
 
 	return &ports.Data, err
+}
+
+// DeletePhysicalPort creates a physical port in Autonomi platform. As
+func (c *Client) DeletePhysicalPort(ctx context.Context, physicalPortID string) error {
+
+	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/accounts/%s/ports/%s", c.hostURL, c.accountID, physicalPortID), nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.doRequest(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
